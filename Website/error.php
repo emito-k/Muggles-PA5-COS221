df72faa8774dec678745442876dd9847786f08bd
sovaria10 <89101174+sovaria10@users.noreply.github.com>
2022-06-07 22:40:15 +0200

    Add files via upload

---
+++ Website/error.php
@@ -0,0 +1,25 @@
+<?php
+// Include the database connection file
+include_once("config.php");
+?>
+
+<html>
+    <head>
+        <title>Muggle - NetballNet</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+    <body>
+        <?php
+            include "./navbar.php";
+        ?>
+
+        <div class="w3-container" style="margin-left:130px; padding-top:50px;">
+            <h1>Seems like something went wrong...</h1>
+            <hr>
+            <br>
+            <p>Go back to <a href="index.php"> home page.</a></p>
+            <p>Refresh the page.</p>
+            <p>Make sure you have a secure internet connection.</p>
+        </div>
+    </body>
+</html>
