e313da7eaf479b275f9bfb4eb5d1ed2a73b112ad
sovaria10 <89101174+sovaria10@users.noreply.github.com>
2022-06-07 02:44:16 +0200

    Add files via upload

---
+++ Website/player.php
@@ -0,0 +1,53 @@
+<?php
+// Include the database connection file
+include_once("config.php");
+?>
+
+<html>
+    <head>
+        <title>Muggle - NetballNet</title>
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+    <body>
+        <?php
+            include "./navbar.php";
+        ?>
+        <h1>Create Player</h1>
+        <hr>
+        <br>
+        <form>
+            <label for="pfp"><b>Upload a profile picture</b></label>
+            <input type="file" id="myFile" name="filename"><br><br>
+
+            <label for="fullname"><b>Fullname</b></label>
+            <input type="input" placeholder="Enter Fullname" name="fullname" required><br><br>
+
+            <label for="birthday"><b>Birthday</b></label>
+            <input type="date" id="birthday" name="birthday" required><br><br>
+
+            <label for="gender"><b>Gender</b></label>
+            <select name="gender" id="gender">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+            </select><br><br>
+
+            <label for="role"><b>Role</b></label>
+            <input type="input" placeholder="Enter Role" name="role" required><br><br>
+
+            <label for="pos"><b>Position</b></label>
+            <input type="input" placeholder="Enter Position" name="pos" required><br><br>
+
+            <label for="uniform_nr"><b>Uniform Number</b></label>
+            <input type="input" placeholder="Enter Uniform Number" name="uniform_nr" required><br><br>
+
+            <label for="height"><b>Height (m)</b></label>
+            <input type="number" id="height" name="height" min="1"><br><br>
+
+            <label for="weight"><b>Weight (kg)</b></label>
+            <input type="number" id="weight" name="weight" min="1"><br><br>
+
+            <input type="submit">
+        </form>
+    </body>
+</html>
